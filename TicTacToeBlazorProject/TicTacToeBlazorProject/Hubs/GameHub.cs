ecdb3fffa9100b1355cfcf69079cf007139bad7e
Matvey <sinema3d00@gmail.com>
2024-09-02 14:23:25 +0300

    making model logic and connection

--- TicTacToeBlazorProject/TicTacToeBlazorProject/Hubs/GameHub.cs
+++ TicTacToeBlazorProject/TicTacToeBlazorProject/Hubs/GameHub.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using TicTacToeBlazorProject.Shared;
 
 namespace TicTacToeBlazorProject.Hubs
 {
     public class GameHub : Hub
     {
-        public override Task OnConnectedAsync()
+        private static readonly List<GameRoom> _rooms = new();
+
+
+        public override async Task OnConnectedAsync()
         {
             Console.WriteLine
                 ($"Player Id: '{Context.ConnectionId}' connected.");
+            await Clients.Caller.SendAsync("Rooms", _rooms);
+            
             return base.OnConnectedAsync();
         }
+
+
     }
 }
