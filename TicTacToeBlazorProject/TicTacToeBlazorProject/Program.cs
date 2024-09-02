be03aab0222835aaa53a417b72c521208ec3a59f
Matvey <sinema3d00@gmail.com>
2024-09-02 13:53:56 +0300

    making client

--- TicTacToeBlazorProject/TicTacToeBlazorProject/Program.cs
+++ TicTacToeBlazorProject/TicTacToeBlazorProject/Program.cs
@@ -1,11 +1,14 @@
 using TicTacToeBlazorProject.Client.Pages;
 using TicTacToeBlazorProject.Components;
+using TicTacToeBlazorProject.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -25,6 +28,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapHub<GameHub>("/gamehub");
 
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
