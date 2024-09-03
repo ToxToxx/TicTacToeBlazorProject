8cc1e8ea00cc0a40c7c2aee8ec6f3a89b5b4968a
Matvey <sinema3d00@gmail.com>
2024-09-03 09:33:57 +0300

    add properties and initialize board method

--- TicTacToeBlazorProject.Shared/TicTacToeGame.cs
+++ TicTacToeBlazorProject.Shared/TicTacToeGame.cs
@@ -10,5 +10,33 @@ namespace TicTacToeBlazorProject.Shared
     {
         public string? PlayerXId { get; set; }
         public string? PlayerOId { get; set; }
+        public string? CurrentPlayerId { get; set; }
+        public string CurrentPlayerSymbol =>
+            CurrentPlayerId == PlayerXId ? "X" : "O";
+        public bool GameStarted { get; set; } = false;
+        public bool GameOver { get; set; } = false;
+        public bool IsDraw {  get; set; } = false;
+        public string Winner {  get; set; } = string.Empty;
+        public List<List<string>> Board {  get; set; } 
+            = new List<List<string>>(3);
+
+        public TicTacToeGame()
+        {
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            Board.Clear();
+            for(int i = 0; i < 3; i++)
+            {
+                var row = new List<string>(3);
+                for (int j = 0; j < 3; j++)
+                {
+                    row.Add(string.Empty);
+                }
+                Board.Add(row);
+            }
+        }
     }
 }
